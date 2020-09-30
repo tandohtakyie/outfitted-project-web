d29e4b7d1864f3b40fdeb610f579daeb2891b54f
mainpc <Arjan.Golabijan@hva.nl>
2020-09-30 15:57:38 +0200

    Add employee

---
+++ src/components/AdminPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="admin-panel">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+        <td>{{ employee.name }}</td>
+        <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'admin-panel',
+    props: {
+    employees: Array,
+    }
+  }
+</script>
+
+<style scoped></style>
---
+++ src/components/EmployerForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="employer-form">
+  <form @submit.prevent="manageSubmit">
+        <label>Employee name</label>
+        <input v-model="employee.name" type="text" />
+        <label>Employee email</label>
+        <input v-model="employee.email" type="text" />
+        <button>Add Employee</button>
+     </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employer-form',
+    data() {
+      return {
+       employee: {
+    name: '',
+    email: '',
+       },
+    }
+  },
+  methods:{
+     manageSubmit(){
+     this.$emit('add:employee',this.employee)
+     },
+
+   },
+ }
+</script>
+
+<style scoped>
+   form{
+    margin-bottom: 2rem;
+   }
+</style>
