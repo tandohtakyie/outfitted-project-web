7c7b72f39e4dbf9074373d4761303590941efa59
mainpc <Arjan.Golabijan@hva.nl>
2020-09-30 15:56:32 +0200

    One giant leap man

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employee List</h1>
+    <employer-form @add:employee="createEmployee"/>
+    <admin-panel :employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AdminPanel from '@/components/AdminPanel.vue'
+import EmployerForm from '@/components/EmployerForm.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    AdminPanel,
+    EmployerForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Terence Martin',
+          email: 't.martin@outfited.com'
+        },
+        {
+          id: 2,
+          name: 'Peppe Hiram',
+          email: 'p.hiram@outfited.com'
+        },
+        {
+          id: 3,
+          name: 'Alan Natalia',
+          email: 'a.natalia@outfited.com'
+        },
+        {
+          id: 4,
+          name: 'Ratmir Viktoria',
+          email: 'r.viktoria@outfited.com'
+        },
+      ],
+    }
+  },
+
+  methods: {
+    createEmployee(employee) {
+      const PreviousId = this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const employeeId = PreviousId + 1;
+      const newEmployee = {...employee, employeeId};
+
+      this.employees = [...this.employees, newEmployee];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 600px;
 }
 </style>
