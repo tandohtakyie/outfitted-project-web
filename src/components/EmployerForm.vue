80610e8c4fe0dd8fa3ec02d499b2230eb4397d0a
mainpc <Arjan.Golabijan@hva.nl>
2020-10-02 12:56:04 +0200

    Correct input check

--- src/components/EmployerForm.vue
+++ src/components/EmployerForm.vue
@@ -2,10 +2,24 @@
   <div id="employer-form">
   <form @submit.prevent="manageSubmit">
         <label>Employee name</label>
-        <input v-model="employee.name" type="text" />
+        <input
+            v-model="employee.name"
+            type="text"
+            :class="{ 'has-error': submission && emptyName }"
+            @focus="clearStatus"
+            @keypress="clearStatus"
+        />
         <label>Employee email</label>
-        <input v-model="employee.email" type="text" />
-        <button>Add Employee</button>
+        <input v-model="employee.email"
+               type="text"
+               :class="{ 'has-error': submission && emptyEmail }"
+               @focus="clearStatus"
+        />
+    <p v-if="failure && submission" class="failure-message">
+      Please fill out the required fields ! </p>
+    <p v-if="success" class="acceptance-message">Employee has been successfully added!</p>
+
+    <button>Add Employee</button>
      </form>
   </div>
 </template>
@@ -15,23 +29,67 @@
     name: 'employer-form',
     data() {
       return {
-       employee: {
-    name: '',
-    email: '',
-       },
-    }
-  },
-  methods:{
-     manageSubmit(){
-     this.$emit('add:employee',this.employee)
-     },
+        submission: false,
+        failure: false,
+        success: false,
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+    methods: {
+      manageSubmit() {
+        this.submission = true
+        this.clearStatus()
 
+        if (this.emptyName || this.emptyEmail) {
+
+          this.failure = true
+          return
+        }
+
+        this.$emit('add:employee', this.employee)
+        this.employee = {
+          name: "",
+          email: "",
+        }
+        this.submission = false
+        this.failure = false
+        this.success = true
+      },
+
+    clearStatus() {
+      this.success = false
+      this.failure = false
+    }
    },
+    computed: {
+      emptyName() {
+        return this.employee.name === ''
+      },
+
+      emptyEmail() {
+        return this.employee.email === ''
+      },
+    },
  }
 </script>
 
 <style scoped>
-   form{
-    margin-bottom: 2rem;
-   }
+
+[class*='-notification'] {
+  font-weight: 400;
+}
+
+form {
+  margin-bottom: 5rem;
+}
+
+.acceptance-message {
+  color: #32a95d;
+}
+.failure-message {
+  color: #d33c40;
+}
 </style>
