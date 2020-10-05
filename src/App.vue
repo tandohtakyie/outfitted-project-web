5ef941c1beae657ca834bf3a3ed8806b29987613
mainpc <Arjan.Golabijan@hva.nl>
2020-10-05 16:54:18 +0200

    Footer fix

--- src/App.vue
+++ src/App.vue
@@ -2,87 +2,30 @@
   <div id="main-app">
     <navigationbar></navigationbar>
     <router-view></router-view>
-    <footer></footer>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 
-import firebase from "firebase";
 import navigationbar from "@/components/Navigationbar";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'app',
   components: {
     navigationbar,
-
-
+    Footer
   },
   data() {
     return {
 
-      employees: [
-
-      ],
-    }
-  },
-
-  mounted() {
-    this.getAllEmployees()
-  },
-
-  methods: {
-    async getAllEmployees() {
-      try {
-        var db = firebase.firestore();
-        const accounts = db.collection('accounts');
-        const snapshot = await accounts.get();
-        snapshot.forEach(doc => {
-          this.employees = [...this.employees, doc.data()]
-        });
-      } catch (error) {
-        console.error(error)
-      }
-    },
-
-    async createEmployee(employee) {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-          method: 'POST',
-          body: JSON.stringify(employee),
-          headers: {'Content-type': 'application/json; charset=UTF-8'},
-        })
-        const data = await response.json()
-        this.employees = [...this.employees, data]
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    async deleteEmployee(id) {
-      try {
-        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-          method: "DELETE"
-        });
-        this.employees = this.employees.filter(employee => employee.id !== id);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async editEmployee(id, updatedEmployee) {
-      try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-          method: 'PUT',
-          body: JSON.stringify(updatedEmployee),
-          headers: {'Content-type': 'application/json; charset=UTF-8'},
-        })
-        const data = await response.json()
-        this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
-      } catch (error) {
-        console.error(error)
-      }
     }
   }
 }
+
+
+
 </script>
 
 <style>
