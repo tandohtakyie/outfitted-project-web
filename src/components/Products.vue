a501db8d22c69cfe6b417a8a0b0b1477aba4766a
Yessin el Khaldi <yessin_khaldi@hotmail.com>
2020-10-05 15:05:43 +0200

    product table template

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,36 @@
 <template>
-<div>Products</div>
+<div>
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th scope="col">ProductId</th>
+      <th scope="col">Name</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    </tbody>
+  </table></div>
+
 </template>
 
 <script>
