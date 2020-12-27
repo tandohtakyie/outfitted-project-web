750d03a2d3aa501669963b284f53312636f80ad3
Arjan <arjan.golabijan@hva.nl>
2020-12-27 20:52:17 +0100

    Added statistics page with one working chart

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,11 +1,42 @@
 <template>
-  <div><h1>DASHBOARD COMING SOON</h1>
+  <div>
+    <HomeStatistics
+      :products="products" v-if="products.length"
+    />
   </div>
 </template>
 
 <script>
+import HomeStatistics from "@/components/Home/HomeStatistics";
+import firebase from "firebase";
+
 export default {
-name: "Home"
+name: "Home",
+  components: {
+  HomeStatistics,
+},  data() {
+    return {
+      products: [],
+    }
+  },
+  mounted() {
+    this.getAllProducts();
+  },
+  methods: {
+    async getAllProducts() {
+      try {
+        var db = firebase.firestore();
+        const products = db.collection('products');
+        const snapshot = await products.get();
+        snapshot.forEach(doc => {
+          this.products = [...this.products, doc.data()]
+        });
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  }
+
 }
 </script>
 
---
+++ src/components/Home/HomeStatistics.vue
@@ -0,0 +1,55 @@
+<template>
+  <div><h1>DASHBOARD COMING SOON</h1>
+    <p v-if="lowStockProductArray.length < 1" class="empty-table">No stock found</p>
+    <div v-else>
+      <column-chart :data="lowStockProductArray"/>
+<!--      <column-chart >-->
+<!--      <tr v-for="product in lowStockProductArray" :key="product.id">-->
+<!--        :data="[['product.name', product.stock]]"-->
+<!--      </tr>-->
+<!--      </column-chart>-->
+<!--      </div>-->
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+import Vue from "vue";
+Vue.use(Chartkick.use(Chart))
+
+export default {
+  name: "HomeStatistics",
+  data() {
+    return {
+      lowStockProductArray: [],
+    }
+  },
+  created() {
+    this.loadLowStockProduct();
+  },
+  props: {
+    products: Array,
+  },
+
+  methods: {
+
+    loadLowStockProduct() {
+     var index2 = 0;
+      for (var index in this.products) {
+        if (this.products[index].stock < 30) {
+          this.lowStockProductArray[index2] = [this.products[index].name, this.products[index].stock ] ;
+          index2++;
+        }
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
